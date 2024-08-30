8b1127c1a7fb3d33466e234b4edc40ee3a53fa16
Thiago C Silva <tkreserva@gmail.com>
2024-08-30 18:41:56 -0300

    Muitl App - deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     localStorage.setItem('autentico',false);}
   return (
     <>
-      <Navegacao />
+      {autentico == "true" && <Navegacao />}
       {autentico == "true" ? <Outlet /> : <Login />}
     </>
   )
